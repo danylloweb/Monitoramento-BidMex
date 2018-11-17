911071e0f88bbed4b78426c4a2b555939b9d60a8
Danyllo7622 <danyllophp@gmail.com>
2018-11-17 10:12:18 -0200

    Route open

--- start/routes.js
+++ start/routes.js
@@ -12,24 +12,24 @@ Route.post('/oauth/token','AuthController.authenticate');
 Route.get('/oauth/revoke-token','AuthController.revokeToken').middleware(['auth']);
 Route.get('/oauth/revoke-tokens-all','AuthController.revokeAllTokens').middleware(['auth']);
 // Teste de funcionamento 
-Route.get('/buscaaereo/back-end','BuscaAereoController.GatewayBuscador').middleware(['auth']);
-Route.get('/buscaaereo/Manager','BuscaAereoController.GatewayManager').middleware(['auth']);
-Route.get('/crawler','CrawlerController.index').middleware(['auth']);
-Route.get('/crm','CrmController.index').middleware(['auth']);
-Route.get('/financial','FinanceiraController.index').middleware(['auth']);
+Route.get('/buscaaereo/back-end','BuscaAereoController.GatewayBuscador');
+Route.get('/buscaaereo/Manager','BuscaAereoController.GatewayManager');
+Route.get('/crawler','CrawlerController.index');
+Route.get('/crm','CrmController.index');
+Route.get('/financial','FinanceiraController.index');
 // Usuarios logados 
-Route.get('/users/logged','BuscaAereoController.usersLoggedIn').middleware(['auth']);
-Route.get('/user/getLastLogin','BuscaAereoController.lastLogin').middleware(['auth']);
+Route.get('/users/logged','BuscaAereoController.usersLoggedIn');
+Route.get('/user/getLastLogin','BuscaAereoController.lastLogin');
 // ApiGateway
-Route.post('/issues/store','IssueController.store').middleware(['auth']);
-Route.get('/issues','IssueController.index').middleware(['auth']);
-Route.get('/issuesByEmissionId','IssueController.issuesByEmission').middleware(['auth']);
+Route.post('/issues/store','IssueController.store');
+Route.get('/issues','IssueController.index');
+Route.get('/issuesByEmissionId','IssueController.issuesByEmission');
 // Direto no busca
-Route.get('/lateOps','EmissionController.index').middleware(['auth']);
-Route.get('/lateOpsFinancial','EmissionController.index').middleware(['auth']);
-Route.get('/dashboards/search-count','EmissionController.index').middleware(['auth']);
-Route.get('/dashboards/op-count','EmissionController.index').middleware(['auth']);
-Route.get('/companies','EmissionController.index').middleware(['auth']);
-Route.get('/averageEmissionTime','EmissionController.index').middleware(['auth']);
+Route.get('/lateOps','EmissionController.index');
+Route.get('/lateOpsFinancial','EmissionController.index');
+Route.get('/dashboards/search-count','EmissionController.index');
+Route.get('/dashboards/op-count','EmissionController.index');
+Route.get('/companies','EmissionController.index');
+Route.get('/averageEmissionTime','EmissionController.index');
 
 Route.any('*', ({ view }) => view.render('home'));
